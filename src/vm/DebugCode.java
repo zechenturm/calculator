619a8a987d53c7d499b3a044a727b3e752e33477
Moritz Lammerich <moritz.lammerich@ost.ch>
2020-05-21 17:56:02 +0200

    add '!' in front of DebugCode debug output if instructions are ignored

--- src/vm/DebugCode.java
+++ src/vm/DebugCode.java
@@ -3,36 +3,42 @@ package vm;
 public class DebugCode extends Interpreter {
     @Override
     public void add() {
+        printIgnored();
         System.out.println("add");
         super.add();
     }
 
     @Override
     public void sub() {
+        printIgnored();
         System.out.println("sub");
         super.sub();
     }
 
     @Override
     public void mul() {
+        printIgnored();
         System.out.println("mul");
         super.mul();
     }
 
     @Override
     public void div() {
+        printIgnored();
         System.out.println("div");
         super.div();
     }
 
     @Override
     public void push(int value) {
+        printIgnored();
         System.out.println("push " + value);
         super.push(value);
     }
 
     @Override
     public int pop() {
+        printIgnored();
         var res = super.pop();
         System.out.println("pop " + res);
         return res;
@@ -40,18 +46,21 @@ public class DebugCode extends Interpreter {
 
     @Override
     public void load(int index) {
+        printIgnored();
         System.out.println("load " + index);
         super.load(index);
     }
 
     @Override
-    public void store(int index, int value) {
-        System.out.println("store " + value + " at " + index);
-        super.store(index, value);
+    public void store(int index) {
+        printIgnored();
+        System.out.println("store " + index);
+        super.store(index);
     }
 
     @Override
     public void branchIfZero(int offset) {
+        printIgnored();
         System.out.println("brIf0 " +  offset);
         super.branchIfZero(offset);
     }
@@ -60,6 +69,12 @@ public class DebugCode extends Interpreter {
     public void label(int index) {
         System.out.println("label " +  index);
         super.label(index);
+    }
+
+    private void printIgnored()
+    {
+        if (ignore)
+            System.out.print("!");
     }
 
 
--- src/vm/Interpreter.java
+++ src/vm/Interpreter.java
@@ -8,7 +8,7 @@ public class Interpreter implements VM
         Stack<Integer> stack = new Stack<>();
         ArrayList<Integer> vars = new ArrayList<>();
 
-        private boolean ignore = false;
+        protected boolean ignore = false;
 
     public void add()
     {
