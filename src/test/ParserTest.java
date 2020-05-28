8387abd2a64e0b7ea3197a44eac1b79d43e642f8
Moritz Lammerich <moritz.lammerich@ost.ch>
2020-05-28 14:15:10 +0200

    clean up ParserTest

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -3,23 +3,21 @@ package test;
 import lexer.Lexer;
 import org.junit.jupiter.api.Test;
 import parser.Parser;
-import vm.DebugCode;
 import vm.Interpreter;
-import vm.VM;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest
 {
-    private static class Parser
+    private static class TestParser
     {
-        parser.Parser p;
+        Parser p;
         Interpreter i;
 
-        public Parser(Lexer l)
+        public TestParser(Lexer l)
         {
             i = new Interpreter();
-            p = new parser.Parser(l, i);
+            p = new Parser(l, i);
         }
 
         public int eval()
@@ -32,7 +30,7 @@ public class ParserTest
     public void TestCreate()
     {
         var l = new Lexer("");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(0, r);
     }
@@ -44,7 +42,7 @@ public class ParserTest
         {
             var num = (int) (Math.random()*1e6);
             var l = new Lexer(Integer.toString(num));
-            var p = new Parser(l);
+            var p = new TestParser(l);
             var r = p.eval();
             assertEquals(num, r);
         }
@@ -54,11 +52,11 @@ public class ParserTest
     public void testSimpleAdd()
     {
         var l = new Lexer("1+1");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(2, r);
         l = new Lexer("1+2");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
         assertEquals(3, r);
     }
@@ -67,11 +65,11 @@ public class ParserTest
     public void testSimpleSubtract()
     {
         var l = new Lexer("1-2");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(-1, r);
         l = new Lexer("1-5");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
         assertEquals(-4, r);
     }
@@ -80,11 +78,11 @@ public class ParserTest
     public void testSimpleMultiply()
     {
         var l = new Lexer("2*2");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(4, r);
         l = new Lexer("2*-5");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
         assertEquals(-10, r);
     }
@@ -93,11 +91,11 @@ public class ParserTest
     public void testSimpleDivide()
     {
         var l = new Lexer("6/2");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(3, r);
         l = new Lexer("10/-5");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
         assertEquals(-2, r);
     }
@@ -106,7 +104,7 @@ public class ParserTest
     public void testMoreComplexExressions()
     {
         var l = new Lexer("16 / 4 * 5");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(20, r);
     }
@@ -115,41 +113,41 @@ public class ParserTest
     public void testOperatorPrecedence()
     {
         var l = new Lexer("4 + 16 / 4");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         var r = p.eval();
         assertEquals(8, r);
 
         l = new Lexer("5 + 16 / 4");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
         assertEquals(9, r);
 
         l = new Lexer("4 + 16 / 4 / 2");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
 
         assertEquals(6, r);
 
         l = new Lexer("4 + 16 / 4 * 3");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
 
         assertEquals(16, r);
 
         l = new Lexer("4 + 9 * 4 / 3");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
 
         assertEquals(16, r);
 
         l = new Lexer("4 - 9 * 4 / 3");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
 
         assertEquals(-8, r);
 
         l = new Lexer("4 - 9 * 4 / 3 + 9 / 3");
-        p = new Parser(l);
+        p = new TestParser(l);
         r = p.eval();
 
         assertEquals(-5, r);
@@ -160,11 +158,11 @@ public class ParserTest
     public void testParens()
     {
         var l = new Lexer("1+(2+3)");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         assertEquals(6, p.eval());
 
         l = new Lexer("5*(2+3)");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(25, p.eval());
     }
 
@@ -172,27 +170,27 @@ public class ParserTest
     public void testAssign()
     {
         var l = new Lexer("x = 1; y = 2; x");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         assertEquals(1, p.eval());
 
         l = new Lexer("x = 1; y = 2; y");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(2, p.eval());
 
         l = new Lexer("x = 1; y = 2; y + x");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(3, p.eval());
 
         l = new Lexer("x = 1; y = x; y");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(1, p.eval());
 
         l = new Lexer("x = 0; x = 1 + 1; x");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(2, p.eval());
 
         l = new Lexer("x = 4; 1+4*x");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(17, p.eval());
     }
 
@@ -200,27 +198,27 @@ public class ParserTest
     public void testIf()
     {
         var l = new Lexer("if 0 10");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         assertEquals(0, p.eval());
 
         l = new Lexer("if 1 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("if (0+1) 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("if 1 10 15");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(15, p.eval());
 
         l = new Lexer("x = 0; if 0 x = 10; x");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(0, p.eval());
 
         l = new Lexer("x = 0; if 1 x = 10; x");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(10, p.eval());
     }
 
@@ -228,31 +226,31 @@ public class ParserTest
     public void testNestedIfs()
     {
         var l = new Lexer("if 1 if 1 10");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("if 1 if 0 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(0, p.eval());
 
         l = new Lexer("if 0 if 1 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(0, p.eval());
 
         l = new Lexer("x = 0; y = 0; z = 2; if x (if y z = 10; z = z + 10;) z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(2, p.eval());
 
         l = new Lexer("x = 0; y = 1; z = 2; if x (if y z = 10; z = z + 10;) z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(2, p.eval());
 
         l = new Lexer("x = 1; y = 0; z = 2; if x (if y z = 10; z = z + 10;) z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(12, p.eval());
 
         l = new Lexer("x = 1; y = 1; z = 2; if x (if y z = 10; z = z + 10;) z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(20, p.eval());
     }
 
@@ -260,31 +258,31 @@ public class ParserTest
     public void testIfElse()
     {
         var l = new Lexer("if 0 1 else 10");
-        var p = new Parser(l);
+        var p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("if 1 1 else 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(1, p.eval());
 
         l = new Lexer("if 1 if 0 20 else 15 else 10");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(15, p.eval());
 
         l = new Lexer("x = 1; y = 0; if x y = 10; else y = y + 10; y");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("x = 0; y = 2; if x y = 10; else y = y + 10; y");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(12, p.eval());
 
         l = new Lexer("x = 1; y = 1; z = 2; if x if y z = 10; else z = z + 10; z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(10, p.eval());
 
         l = new Lexer("x = 1; y = 0; z = 2; if x if y z = 10; else z = z + 10; z");
-        p = new Parser(l);
+        p = new TestParser(l);
         assertEquals(12, p.eval());
     }
 }
