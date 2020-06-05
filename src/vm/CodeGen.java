6aced11033e00ab5423f502507bb76a606146372
Moritz Lammerich <moritz.lammerich@ost.ch>
2020-06-05 17:49:27 +0200

    add getStackSize() to CodeGen
    getStackSize() return the stack size needed to run the program. This can be
    used by VM's/implementations to pre allocate the exact amount of stack space
    needed.

--- src/vm/CodeGen.java
+++ src/vm/CodeGen.java
@@ -9,6 +9,9 @@ public class CodeGen implements AbstractMachine
     private ArrayList<ByteCode> code = new ArrayList<>();
     private final FunctionSignature[] builtins;
 
+    private int stackTop = 0;
+    private int stackSize = 0;
+
     public CodeGen(FunctionSignature[] builtinFunctions)
     {
         builtins = builtinFunctions;
@@ -21,21 +24,27 @@ public class CodeGen implements AbstractMachine
 
     public void loadValue(int value)
     {
+        calcStackSize(1);
         code.add(new ByteCode(ByteCode.Type.LOAD_VALUE, value));
     }
 
     @Override
-    public void load(int index) {
+    public void load(int index)
+    {
+        calcStackSize(1);
         code.add(new ByteCode(ByteCode.Type.LOAD, index));
     }
 
     @Override
     public void store(int index) {
+        calcStackSize(-1);
         code.add(new ByteCode(ByteCode.Type.STORE, index));
     }
 
     @Override
-    public void branchIfZero(int label) {
+    public void branchIfZero(int label)
+    {
+        calcStackSize(-1);
         code.add(new ByteCode(ByteCode.Type.BR_IF_0, label));
     }
 
@@ -51,26 +60,32 @@ public class CodeGen implements AbstractMachine
 
     @Override
     public void call(int index) {
+        calcStackSize(1 - builtins[index].numArgs);
         code.add(new ByteCode(ByteCode.Type.CALL, index));
     }
 
     public void add()
     {
+        calcStackSize(-2);
         code.add(new ByteCode(ByteCode.Type.ADD, 0));
     }
 
     @Override
-    public void subtract() {
+    public void subtract()
+    {
+        calcStackSize(-2);
         code.add(new ByteCode(ByteCode.Type.SUB, 0));
     }
 
     public void multiply()
     {
+        calcStackSize(-2);
         code.add(new ByteCode(ByteCode.Type.MUL, 0));
     }
 
     public void divide()
     {
+        calcStackSize(-2);
         code.add(new ByteCode(ByteCode.Type.DIV, 0));
     }
 
@@ -83,5 +98,15 @@ public class CodeGen implements AbstractMachine
     {
         var a = new ByteCode[code.size()];
         return code.toArray(a);
+    }
+
+    public int getStackSize() {
+        return stackSize;
+    }
+
+    private void calcStackSize(int change)
+    {
+        stackTop += change;
+        stackSize = Math.max(stackSize, stackTop);
     }
 }
